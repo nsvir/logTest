d2b8bd7ffb50b5b73de9e84bfdbcafbbb6de20ec
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2017-10-28 16:06:00 +0200

    Refractoring

--- SpiralsLogTest.py
+++ SpiralsLogTest.py
@@ -7,30 +7,36 @@ LogInfo = lambda *args : None
 class AssertFailedException(Exception):
     pass
 
+
+
 class Application:
 
     def __init__(self):
         self.logFiles = []
+        self.errors = []
 
     def register(self, logFile):
         self.logFiles.append(logFile)
 
     def verify(self):
-        successLogs = 0
-        errors = []
         for logFile in self.logFiles:
             try:
                 logFile.verify()
-                successLogs += 1
             except AssertFailedException as e:
-                errors.append({"file": logFile.fileName, "args": e.args})
+                self.errors.append({"file": logFile.fileName, "args": e.args})
+        self._printResult()
+
+    def _printResult(self):
         LogInfo("\nSummary:\n")
-        myprint = printOk if (len(errors) == 0) else printFail
+        total = len(self.logFiles)
+        failure = len(self.errors)
+        success = total - failure
+        myprint = printOk if failure == 0 else printFail
         myprint("Success: {0}\tFailed: {1}\t Total: {2}\n".format(
-            successLogs,
-            len(self.logFiles) - successLogs,
-            len(self.logFiles)))
-        for error in errors:
+            success,
+            failure,
+            total))
+        for error in self.errors:
             myprint("{0}: Expected '{1}' from line n°{2} to the end of file\n".format(
                 error["file"],
                 error["args"][0],
@@ -47,12 +53,9 @@ class LogFile(object):
         self.expectedList.append(expected)
 
     def _verifyLogFile(self):
-        global LogSuccess
-        global LogInfo
-        LogInfo("Testing: {0}\n".format(self.fileName))
         fileStream = open(self.fileName, 'r')
-        lineIndex = 0
-        lastLine = 0
+        lineIndex = lastLine = 1
+        LogInfo("Testing: {0}\n".format(self.fileName))
         for expectedLine in self.expectedList:
             for line in fileStream:
                 lineIndex += 1
@@ -60,12 +63,11 @@ class LogFile(object):
                     LogSuccess("{0}:{1}".format(lineIndex, line))
                     lastLine = lineIndex
                     break # Go to next expected item
-            else:
+            else: # End of file reached but expected line not find
                 LogFailure("{0}\n".format(expectedLine))
                 fileStream.close()
                 raise AssertFailedException(expectedLine, lastLine)
         fileStream.close()
-
 
     def verify(self):
         try:
